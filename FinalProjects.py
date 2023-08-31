680b793625488e0632a5a9b0f7fdb7830143f1b3
HadeelAbed <hadeeliug@gmail.com>
2023-08-31 15:20:51 +0300

    Task2 commit

--- FinalProjects.py
+++ FinalProjects.py
@@ -1,3 +1,10 @@
 import uuid
 name = input("Enter your name ")
 submission = input("Enter your submission")
+
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()  # Generate a unique course ID
+        self.course_name = course_name
+        self.course_mark = course_mark
+
